4b6240a8b935f73f8e9d0e1be805af5e13d310be
yerin97 <92047632+yerin97@users.noreply.github.com>
2021-12-06 17:21:55 -0500

    Update linear_regression.py

--- pages/linear_regression.py
+++ pages/linear_regression.py
@@ -36,7 +36,7 @@ def app():
     st.markdown("# Let's Predict Your Country")
     st.markdown("We created a linear regression model using 6 features. This model predicts the happiness score based on your provided inputs. \
     Feel free to move the sliders below to provide inputs to our model and predict the happiness score of your country! \
-    Also, our model additionally guesses your country by finding the one which has the most similar happiness score by applying euclidean distance")
+    Also, our model additionally guesses your country by finding the one which has the most similar happiness score by applying euclidean distance.")
     
     gdp  = st.number_input('GDP per Capita')
 
